0f20c77e138e7162782fdbaa2ef3887de44c734e
471427500@qq.com <ghostgood>
2013-12-14 22:23:30 +0800

    提交企讯通android密码

---
+++ android-contact/gen/cn/ppm123/contact/BuildConfig.java
@@ -0,0 +1,8 @@
+/*___Generated_by_IDEA___*/
+
+/** Automatically generated file. DO NOT MODIFY */
+package cn.ppm123.contact;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
